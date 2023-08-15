05c2e0d64ce25a6961ed9e635439aa93478ba781
Mohamedabdullhaleam <mohamedlimo236@gmail.com>
2023-08-15 15:31:34 +0300

    day4

---
+++ days of code/day 4/heads or tails.py
@@ -0,0 +1,14 @@
+import random as rd
+N=rd.random()
+N=round(N)
+# print(N)
+if N == 1 :
+    print("Heads")
+else:
+    print("Tails")
+############## another solution 
+x=rd.randint(0,1)
+if x == 1 :
+    print("Heads")
+else:
+    print("Tails")
